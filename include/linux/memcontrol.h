21e52cdacecbe0570b8530e16537618ed83a8dc4
Yosry Ahmed <yosryahmed@google.com>
2023-03-30 19:17:59 +0000

    UPSTREAM: workingset: memcg: sleep when flushing stats in workingset_refault()
    In workingset_refault(), we call
    mem_cgroup_flush_stats_atomic_ratelimited() to read accurate stats within
    an RCU read section and with sleeping disallowed.  Move the call above the
    RCU read section to make it non-atomic.
    
    Flushing is an expensive operation that scales with the number of cpus and
    the number of cgroups in the system, so avoid doing it atomically where
    possible.
    
    Since workingset_refault() is the only caller of
    mem_cgroup_flush_stats_atomic_ratelimited(), just make it non-atomic, and
    rename it to mem_cgroup_flush_stats_ratelimited().
    
    Link: https://lkml.kernel.org/r/20230330191801.1967435-7-yosryahmed@google.com
    Change-Id: Ia073b9bcef12dfced053ebea7a5da96ce942596c
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Acked-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Josef Bacik <josef@toxicpanda.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vasily Averin <vasily.averin@linux.dev>
    Cc: Zefan Li <lizefan.x@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 4009b2f1887036d30637bc06dd0ade7e18408bb3)
    Bug: 322544714
    Bug: 389094973
    Signed-off-by: T.J. Mercier <tjmercier@google.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1104,7 +1104,7 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 
 void mem_cgroup_flush_stats(void);
 void mem_cgroup_flush_stats_atomic(void);
-void mem_cgroup_flush_stats_atomic_ratelimited(void);
+void mem_cgroup_flush_stats_ratelimited(void);
 
 void __mod_memcg_lruvec_state(struct lruvec *lruvec, enum node_stat_item idx,
 			      int val);
@@ -1602,7 +1602,7 @@ static inline void mem_cgroup_flush_stats_atomic(void)
 {
 }
 
-static inline void mem_cgroup_flush_stats_atomic_ratelimited(void)
+static inline void mem_cgroup_flush_stats_ratelimited(void)
 {
 }
 
