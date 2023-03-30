a47f9149a9c994ff61f82f77a32ee5cdbe0db5dc
Yosry Ahmed <yosryahmed@google.com>
2023-03-30 19:17:55 +0000

    UPSTREAM: memcg: rename mem_cgroup_flush_stats_"delayed" to "ratelimited"
    mem_cgroup_flush_stats_delayed() suggests his is using a delayed_work, but
    this is actually sometimes flushing directly from the callsite.
    
    What it's doing is ratelimited calls.  A better name would be
    mem_cgroup_flush_stats_ratelimited().
    
    Link: https://lkml.kernel.org/r/20230330191801.1967435-3-yosryahmed@google.com
    Change-Id: Ib418ae17599d10c520b766f0c0e4396a43906256
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Suggested-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Josef Bacik <josef@toxicpanda.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vasily Averin <vasily.averin@linux.dev>
    Cc: Zefan Li <lizefan.x@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 92fbbc7202ac4fb16250dc91c88211814ae2190b)
    Bug: 322544714
    Bug: 389094973
    Signed-off-by: T.J. Mercier <tjmercier@google.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1103,7 +1103,7 @@ static inline unsigned long lruvec_page_state_local(struct lruvec *lruvec,
 }
 
 void mem_cgroup_flush_stats(void);
-void mem_cgroup_flush_stats_delayed(void);
+void mem_cgroup_flush_stats_ratelimited(void);
 
 void __mod_memcg_lruvec_state(struct lruvec *lruvec, enum node_stat_item idx,
 			      int val);
@@ -1597,7 +1597,7 @@ static inline void mem_cgroup_flush_stats(void)
 {
 }
 
-static inline void mem_cgroup_flush_stats_delayed(void)
+static inline void mem_cgroup_flush_stats_ratelimited(void)
 {
 }
 
