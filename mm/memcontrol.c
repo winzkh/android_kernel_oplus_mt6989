c09279957446dbc1c3db8c2b5f1495c93a615aec
Yosry Ahmed <yosryahmed@google.com>
2023-03-30 19:17:56 +0000

    UPSTREAM: memcg: do not flush stats in irq context
    Currently, the only context in which we can invoke an rstat flush from irq
    context is through mem_cgroup_usage() on the root memcg when called from
    memcg_check_events().  An rstat flush is an expensive operation that
    should not be done in irq context, so do not flush stats and use the stale
    stats in this case.
    
    Arguably, usage threshold events are not reliable on the root memcg anyway
    since its usage is ill-defined.
    
    Link: https://lkml.kernel.org/r/20230330191801.1967435-4-yosryahmed@google.com
    Change-Id: If230311168f126e3741afaeab1f20cb1949190f0
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Suggested-by: Johannes Weiner <hannes@cmpxchg.org>
    Suggested-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Josef Bacik <josef@toxicpanda.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michal Koutný <mkoutny@suse.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vasily Averin <vasily.averin@linux.dev>
    Cc: Zefan Li <lizefan.x@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit a2174e95cce5e65ee49c60368434aaae944ff1af)
    Bug: 322544714
    Signed-off-by: T.J. Mercier <tjmercier@google.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3758,11 +3758,22 @@ static unsigned long mem_cgroup_usage(struct mem_cgroup *memcg, bool swap)
 
 	if (mem_cgroup_is_root(memcg)) {
 		/*
-		 * Approximate root's usage from global state. This isn't
-		 * perfect, but the root usage was always an approximation.
+		 * We can reach here from irq context through:
+		 * uncharge_batch()
+		 * |--memcg_check_events()
+		 *    |--mem_cgroup_threshold()
+		 *       |--__mem_cgroup_threshold()
+		 *          |--mem_cgroup_usage
+		 *
+		 * rstat flushing is an expensive operation that should not be
+		 * done from irq context; use stale stats in this case.
+		 * Arguably, usage threshold events are not reliable on the root
+		 * memcg anyway since its usage is ill-defined.
 		 */
-		val = global_node_page_state(NR_FILE_PAGES) +
-			global_node_page_state(NR_ANON_MAPPED);
+		if (in_task())
+			mem_cgroup_flush_stats();
+		val = memcg_page_state(memcg, NR_FILE_PAGES) +
+			memcg_page_state(memcg, NR_ANON_MAPPED);
 		if (swap)
 			val += total_swap_pages - get_nr_swap_pages();
 	} else {
