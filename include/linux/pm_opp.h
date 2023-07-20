3d44d1d5f818f673e85d99bd2df480ee24d83113
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2023-07-20 11:10:53 +0530

    OPP: Introduce dev_pm_opp_get_freq_indexed() API
    [ Upstream commit 5f756d03e2c7db63c1df7148d7b1739f29ff1532 ]
    
    In the case of devices with multiple clocks, drivers need to specify the
    frequency index for the OPP framework to get the specific frequency within
    the required OPP. So let's introduce the dev_pm_opp_get_freq_indexed() API
    accepting the frequency index as an argument.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    [ Viresh: Fixed potential access to NULL opp pointer ]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Stable-dep-of: b44b9bc7cab2 ("OPP: fix dev_pm_opp_find_bw_*() when bandwidth table not initialized")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -117,6 +117,8 @@ unsigned long dev_pm_opp_get_power(struct dev_pm_opp *opp);
 
 unsigned long dev_pm_opp_get_freq(struct dev_pm_opp *opp);
 
+unsigned long dev_pm_opp_get_freq_indexed(struct dev_pm_opp *opp, u32 index);
+
 unsigned int dev_pm_opp_get_level(struct dev_pm_opp *opp);
 
 unsigned int dev_pm_opp_get_required_pstate(struct dev_pm_opp *opp,
@@ -222,6 +224,11 @@ static inline unsigned long dev_pm_opp_get_power(struct dev_pm_opp *opp)
 }
 
 static inline unsigned long dev_pm_opp_get_freq(struct dev_pm_opp *opp)
+{
+	return 0;
+}
+
+static inline unsigned long dev_pm_opp_get_freq_indexed(struct dev_pm_opp *opp, u32 index)
 {
 	return 0;
 }
