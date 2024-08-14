8eae53640594a586efbd9e916957bc3929a9d166
Yang Yang <yang.yang@vivo.com>
2024-08-14 18:37:22 +0800

    ANDROID: vendor_hooks: add hook in create_worker()
    This hook is used to apply specific scheduling policies to worker->task
    
    Bug: 360039558
    Bug: 408116496
    Bug: 434145892
    Bug: 435360464
    Change-Id: I4fb79b23bced1c58a6fa5f4d0c19850f11d56e0f
    Signed-off-by: Yang Yang <yang.yang@vivo.com>
    (cherry picked from commit 2041959e88de73c2487def20a89c60a7f8177938)
    Signed-off-by: ying zuxin <11154159@vivo.com>
    (cherry picked from commit 25d43121e3f3902876198cd4deb803ce76b944d3)
    Signed-off-by: renchenghan <renchenghan@vivo.corp-partner.google.com>
    [jstultz: Utilized conflict resolution from https://r.android.com/3706791
     combining this into the pre-existing cherry-pick to andorid14-6.1]
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1997,7 +1997,9 @@ static struct worker *create_worker(struct worker_pool *pool)
 		set_user_nice(worker->task, pool->attrs->nice);
 #else
 	set_user_nice(worker->task, pool->attrs->nice);
-#endif
+
+	/* The hook is placed here because it needs to use the nice value */
+	trace_android_rvh_create_worker(worker->task, pool->attrs);
 	kthread_bind_mask(worker->task, pool->attrs->cpumask);
 
 	/* successful, attach the worker to the pool */
