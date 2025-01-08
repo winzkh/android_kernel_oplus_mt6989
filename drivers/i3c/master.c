c169d292cc787c12466093b9ee1c9815ec297ce3
Frank Li <Frank.Li@nxp.com>
2025-01-08 17:55:33 -0500

    i3c: master: Fix missing 'ret' assignment in set_speed()
    commit b266e0d4dac00eecdfaf50ec3f708fd0c3b39637 upstream.
    
    Fix a probe failure in the i3c master driver that occurs when no i3c
    devices are connected to the bus.
    
    The issue arises in `i3c_master_bus_init()` where the `ret` value is not
    updated after calling `master->ops->set_speed()`. If no devices are
    present, `ret` remains set to `I3C_ERROR_M2`, causing the code to
    incorrectly proceed to `err_bus_cleanup`.
    
    Cc: stable@vger.kernel.org
    Fixes: aef79e189ba2 ("i3c: master: support to adjust first broadcast address speed")
    Signed-off-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Acked-by: Mukesh Kumar Savaliya <quic_msavaliy@quicinc.com>
    Reviewed-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/r/20250108225533.915334-1-Frank.Li@nxp.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/i3c/master.c
+++ drivers/i3c/master.c
@@ -1736,6 +1736,12 @@ static int i3c_master_bus_init(struct i3c_master_controller *master)
 	if (ret && ret != I3C_ERROR_M2)
 		goto err_bus_cleanup;
 
+	if (master->ops->set_speed) {
+		ret = master->ops->set_speed(master, I3C_OPEN_DRAIN_NORMAL_SPEED);
+		if (ret)
+			goto err_bus_cleanup;
+	}
+
 	/* Disable all slave events before starting DAA. */
 	ret = i3c_master_disec_locked(master, I3C_BROADCAST_ADDR,
 				      I3C_CCC_EVENT_SIR | I3C_CCC_EVENT_MR |
