ac688cb781c5be7e912f13da28d9e1a6875f6f66
zhanghui <zhanghui31@xiaomi.com>
2025-02-14 16:23:03 +0800

    ANDROID: mm: add a new vendor hook in filemap_map_pages
    In the current vendor hook, if next_uptodate_folio returns NULL, the
    first_pgoff is set to zero, and the last_pgoff is set to start_pgoff.
    Therefore, the collection range is from 0 to the start_pgoff.
    
    |-----------|------------|-------------|------------------|
    0      start_pgoff  first_pgoff    last_pgoff         end_pgoff
    
    We want to collect the first_pgoff to last_pgoff, so we have to add a
    new vendor hook.
    
    Bug: 398130226
    Bug: 400290767
    Bug: 401380274
    
    Change-Id: I19d54c601e2ffc5de5ec2dafcd43fbdcdc84b0d2
    Signed-off-by: zhanghui <zhanghui31@xiaomi.com>
    (cherry picked from commit eaffa3e34155d13c4a2df6d44e46d8a046c2ea4c)
    (cherry picked from commit 5ef6cde4d9c698f247fa29d6f19bcda74daab4ca)

--- mm/filemap.c
+++ mm/filemap.c
@@ -3575,12 +3575,14 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
         if (trace_filemap_map_pages_enabled())
                 pathbuf = kmalloc(PATH_MAX, GFP_KERNEL);
 #endif
+	pgoff_t orig_start_pgoff = start_pgoff;
 
 	rcu_read_lock();
 	folio = first_map_page(mapping, &xas, end_pgoff);
 	if (!folio)
 		goto out;
 	first_pgoff = xas.xa_index;
+	orig_start_pgoff = xas.xa_index;
 
 	if (filemap_map_pmd(vmf, &folio->page)) {
 		ret = VM_FAULT_NOPAGE;
@@ -3651,6 +3653,7 @@ out:
 #endif
 	WRITE_ONCE(file->f_ra.mmap_miss, mmap_miss);
 	trace_android_vh_filemap_map_pages(file, first_pgoff, last_pgoff, ret);
+	trace_android_vh_filemap_map_pages_range(file, orig_start_pgoff, last_pgoff, ret);
 
 	return ret;
 }
