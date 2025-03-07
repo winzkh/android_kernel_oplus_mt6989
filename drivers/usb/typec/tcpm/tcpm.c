2090342cc95ed54539a911e220c05cc89509f010
Greg Kroah-Hartman <gregkh@google.com>
2025-03-07 09:49:56 +0000

    Revert "usb: typec: tcpci: Prevent Sink disconnection before vPpsShutdown in SPR PPS"
    This reverts commit 495320792721fcc527c36c82ec8e10f46adc8347 which is
    commit 4d27afbf256028a1f54363367f30efc8854433c3 upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I6b743cfeb8ce98af2f957100d6a32ab4ea4bf515
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -2348,9 +2348,12 @@ static int tcpm_set_auto_vbus_discharge_threshold(struct tcpm_port *port,
 	if (!port->tcpc->set_auto_vbus_discharge_threshold)
 		return 0;
 
-	ret = port->tcpc->set_auto_vbus_discharge_threshold(port->tcpc, mode, pps_active,
-							    requested_vbus_voltage,
-							    port->pps_data.min_volt);
+	if (mode == TYPEC_PWR_MODE_PD && pps_active)
+		voltage = port->pps_data.min_volt;
+	else
+		voltage = requested_vbus_voltage;
+
+	ret = port->tcpc->set_auto_vbus_discharge_threshold(port->tcpc, mode, pps_active, voltage);
 	tcpm_log_force(port,
 		       "set_auto_vbus_discharge_threshold mode:%d pps_active:%c vbus:%u pps_apdo_min_volt:%u ret:%d",
 		       mode, pps_active ? 'y' : 'n', requested_vbus_voltage,
