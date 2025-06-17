fa17d0c9ee8b0be3425d68459bd9261cb701fdac
Kuen-Han Tsai <khtsai@google.com>
2025-06-17 13:07:12 +0800

    usb: gadget: u_serial: Fix race condition in TTY wakeup
    commit c529c3730bd09115684644e26bf01ecbd7e2c2c9 upstream.
    
    A race condition occurs when gs_start_io() calls either gs_start_rx() or
    gs_start_tx(), as those functions briefly drop the port_lock for
    usb_ep_queue(). This allows gs_close() and gserial_disconnect() to clear
    port.tty and port_usb, respectively.
    
    Use the null-safe TTY Port helper function to wake up TTY.
    
    Example
      CPU1:			      CPU2:
      gserial_connect() // lock
      			      gs_close() // await lock
      gs_start_rx()     // unlock
      usb_ep_queue()
      			      gs_close() // lock, reset port.tty and unlock
      gs_start_rx()     // lock
      tty_wakeup()      // NPE
    
    Fixes: 35f95fd7f234 ("TTY: usb/u_serial, use tty from tty_port")
    Cc: stable <stable@kernel.org>
    Signed-off-by: Kuen-Han Tsai <khtsai@google.com>
    Reviewed-by: Prashanth K <prashanth.k@oss.qualcomm.com>
    Link: https://lore.kernel.org/linux-usb/20240116141801.396398-1-khtsai@google.com/
    Link: https://lore.kernel.org/r/20250617050844.1848232-2-khtsai@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/u_serial.c
+++ drivers/usb/gadget/function/u_serial.c
@@ -290,8 +290,8 @@ __acquires(&port->port_lock)
 			break;
 	}
 
-	if (do_tty_wake && port->port.tty)
-		tty_wakeup(port->port.tty);
+	if (do_tty_wake)
+		tty_port_tty_wakeup(&port->port);
 	return status;
 }
 
@@ -577,7 +577,7 @@ static int gs_start_io(struct gs_port *port)
 		 * we didn't in gs_start_tx() */
 		if (!port->port.tty)
 			goto out;
-		tty_wakeup(port->port.tty);
+		tty_port_tty_wakeup(&port->port);
 	} else {
 out:
 		/* Free reqs only if we are still connected */
