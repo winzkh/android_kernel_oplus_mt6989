bdaed4cbc084e34f0fdfccfaf7322bd94db414d7
Huan Yang <link@vivo.corp-partner.google.com>
2025-07-30 10:53:06 +0800

    ANDROID: Backport: page_alloc: add vendor hook to try alloc pages
    Add new vendor hook to get pages with gfp from vendor implemented pool.
    Vendor hook trace_android_rvh_try_alloc_pages() was previously added via
    https://android-review.googlesource.com/c/kernel/common/+/3184382\.
    
    But there were missing gfp_t in the commit, which is needed for
    page allocation. So add a new vendor hook with gfp.
    
    Bug: 351175506
    Bug: 435041237
    Bug: 435287181
    
    Change-Id: I64b382b8d6d528d58146f32ebabe2883234f2653
    Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>
    Signed-off-by: Huan Yang <link@vivo.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -478,3 +478,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sdhci_get_cd);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_gpio_cd_irqt);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_filemap_map_pages_range);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_referenced_check_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_try_alloc_pages_gfp);
